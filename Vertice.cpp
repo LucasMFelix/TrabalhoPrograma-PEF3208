6c515984e514d92c7c8df81c4f8af69e5fd3c049
Reinaldo Avelino <73717173+ReinaldoAvelino@users.noreply.github.com>
2021-06-24 18:13:36 -0300

    Update Vertice.cpp

--- Vertice.cpp
+++ Vertice.cpp
@@ -29,3 +29,10 @@ bool Vertice::addForca(double intensidade, char direcao)
     return false;
 }
 
+bool Vertice::addIncognita(double intensidade, char direcao)
+{
+    if (direcao == 'i') {this->incognitas[0] = intensidade; return true;}
+    else if (direcao == 'j') {this->incognitas[1] = intensidade; return true;}
+    else if (direcao == 'k') {this->incognitas[2] = intensidade; return true;}
+    return false;
+}
