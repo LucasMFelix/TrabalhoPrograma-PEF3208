0abdb134fe6f9bec255644aecf66343c70b96a0a
Reinaldo Avelino <73717173+ReinaldoAvelino@users.noreply.github.com>
2021-06-24 18:04:41 -0300

    Update Vertice.h

--- Vertice.h
+++ Vertice.h
@@ -18,7 +18,7 @@ protected:
 	Vertice* adjacentes[MAXIMO_ARESTAS];
 	int quantidade = 0;
 	double forcas[MAXIMO_FORCAS];
-	double incognita[3]; // {Fx, Fy, Mz}
+	double incognitas[3]; // {Fx, Fy, Mz}
 };
 
 #endif // VERTICE_H
