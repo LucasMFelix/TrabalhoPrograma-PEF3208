249d72dcc100f2163a42cf4c55add4517ced61da
LucasMFelix <81572617+LucasMFelix@users.noreply.github.com>
2021-06-26 02:30:28 -0300

    Update Vertice.h

--- Vertice.h
+++ Vertice.h
@@ -1,8 +1,9 @@
 #ifndef VERTICE_H
 #define VERTICE_H
-
+#include "Aresta.h"
 #include <iostream>
 using namespace std;
+class Aresta;
 
 #define MAXIMO_ARESTAS 4 // maximo de arestas por vertice
 
@@ -16,9 +17,15 @@ public:
 	double* getForcas();
 	bool setIncognita(int booleana, char direcao); // direcao pode ser 'i', 'j'
 	int* getIncognitas();
+	char** getArestas();
+	bool adicionarAresta(char* nomeAresta);
+	bool setCor(string novaCor);
+	string getCor();
 protected:
+    string cor = "BRANCO"; //Pode ser branco, preto ou cinza
 	char nome;
-	Vertice* adjacentes[MAXIMO_ARESTAS];
+	char* arestasPertencentes[MAXIMO_ARESTAS];//Guarda as arestas que o vertice pertence
+	int quantidadeDeArestas = 0;
 	int quantidade = 0;
 	double forcas[2] = {0, 0}; // {Fx, Fy}
 	int incognitas[3] = {0, 0, 0}; // {Fx, Fy, Mz} // 1 se tiver incognita, 0 caso contrario
