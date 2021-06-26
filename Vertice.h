360a8dcb5619d19ca0d79ca3b888d3b7a3552593
LucasMFelix <81572617+LucasMFelix@users.noreply.github.com>
2021-06-26 12:23:45 -0300

    Update Vertice.h

--- Vertice.h
+++ Vertice.h
@@ -12,21 +12,20 @@ public:
 	Vertice(char nome);
 	virtual ~Vertice();
 	char getNome();
-	int getNumeroDeArestrasDeSaida();
+	int getNumeroDeArestras();
 	bool addForca(double intensidade, char direcao); // direcao pode ser 'i' ou 'j'
 	double* getForcas();
 	bool setIncognita(int booleana, char direcao); // direcao pode ser 'i', 'j'
 	int* getIncognitas();
-	char** getArestas();
-	bool adicionarAresta(char* nomeAresta);
+	Aresta** getArestas();
+	bool adicionarAresta(Aresta* aresta);
 	bool setCor(string novaCor);
 	string getCor();
 protected:
     string cor = "BRANCO"; //Pode ser branco, preto ou cinza
 	char nome;
-	char* arestasPertencentes[MAXIMO_ARESTAS];//Guarda as arestas que o vertice pertence
+	Aresta* arestasPertencentes[MAXIMO_ARESTAS];//Guarda as arestas que o vertice pertence
 	int quantidadeDeArestas = 0;
-	int quantidade = 0;
 	double forcas[2] = {0, 0}; // {Fx, Fy}
 	int incognitas[3] = {0, 0, 0}; // {Fx, Fy, Mz} // 1 se tiver incognita, 0 caso contrario
 };
