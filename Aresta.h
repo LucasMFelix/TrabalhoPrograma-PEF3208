4b75bf901eb4a48cc56cc3d9165c0965c8fb139b
Reinaldo Avelino <73717173+ReinaldoAvelino@users.noreply.github.com>
2021-06-27 22:36:02 -0300

    Update Aresta.h

--- Aresta.h
+++ Aresta.h
@@ -1,23 +1,18 @@
 #ifndef ARESTA_H
 #define ARESTA_H
-#include <iostream>
+
 #include "Vertice.h"
-class Vertice;
 
 class Aresta {
 public:
-    Aresta(char* nome, float comprimento, float inclinacao, Vertice* primeiroVertice, Vertice* segundoVertice);
+    Aresta(char* nome, float comprimento, float inclinacao);
     virtual ~Aresta();
     char* getNome();
-    Vertice* getPrimeiroVertice();
-    Vertice* getSegundoVertice();
     void addDistribuicao(double intensidade, int forma); // adiciona uma carga de forca distribuida
                                                          // forma 1 para retangulo e 2 para triangulo
 
 protected:
     char* nome;
-    Vertice* primeiroVertice = NULL;//primeira letra
-    Vertice* segundoVertice = NULL;
     float comprimento;
     float inclinacao; // habilitado somente para 0 ou 90 graus
     double intensidade; // intensidade da forca distribuida
