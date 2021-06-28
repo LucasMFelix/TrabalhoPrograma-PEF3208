cc997e3e967e108a85c3bd4227b62a36e911d2cd
Reinaldo Avelino <73717173+ReinaldoAvelino@users.noreply.github.com>
2021-06-27 21:56:04 -0300

    Update Vertice.h

--- Vertice.h
+++ Vertice.h
@@ -1,10 +1,8 @@
 #ifndef VERTICE_H
 #define VERTICE_H
-#include "Aresta.h"
+
 #include <iostream>
-#include <string>
 using namespace std;
-class Aresta;
 
 #define MAXIMO_ARESTAS 4 // maximo de arestas por vertice
 
@@ -13,22 +11,20 @@ public:
 	Vertice(char nome);
 	virtual ~Vertice();
 	char getNome();
-	int getNumeroDeArestras();
+	int getNumeroDeArestrasDeSaida();
 	bool addForca(double intensidade, char direcao); // direcao pode ser 'i' ou 'j'
 	double* getForcas();
 	bool setIncognita(int booleana, char direcao); // direcao pode ser 'i', 'j'
 	int* getIncognitas();
-	Aresta** getArestas();
-	bool adicionarAresta(Aresta* aresta);
-	bool setCor(string novaCor);
-	string getCor();
+	void setCoordenadas(double x0, double y0);
+	double* getCoordenadas();
 protected:
-    string cor = "BRANCO"; // Pode ser branco, preto ou cinza
 	char nome;
-	Aresta* arestasPertencentes[MAXIMO_ARESTAS]; // Guarda as arestas que o vertice pertence
-	int quantidadeDeArestas = 0;
+	Vertice* adjacentes[MAXIMO_ARESTAS];
+	int quantidade = 0;
 	double forcas[2] = {0, 0}; // {Fx, Fy}
 	int incognitas[3] = {0, 0, 0}; // {Fx, Fy, Mz} // 1 se tiver incognita, 0 caso contrario
+    double coordenadas[2]; // {x0, y0}
 };
 
 #endif // VERTICE_H
